49bb64342e27b7cc9a2eeb93941047f69f186a9b
hanjoon-kim <hanjoon3@gmail.com>
2018-06-11 12:22:58 -0700

    Implemented up to and including arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,69 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  /*
+  input: various arguments of different data types
+  output: string
+  
+  strategy: first identify what data type the argument is
+            then we turn it into a string 
+            Recursion comes in for arrays and objects
+              Array.isArray --> stringify RECURISOn
+              for objects keys have to be strings, properties can be any data type
+              recursion on the property
+            Primitive types are easy peasy
+  if number
+    turn the number into a string
+  if null
+    return 'null'
+  if boolean
+    return respective boolean
+  if string
+    add quotes and return string
+  if array
+    add brackets as string, and in between run stringify for each element
+  if obj
+    add bracket and then add stringified key and then add colon, stringify values
+    add commas as necessary for every single key: value pair
+    add closing bracket
+    return
+    deal with functinos and undefined values
+                
+*/
+  var result;
+
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+  if (obj === null) {
+    return 'null'; 
+  }
+  if (typeof obj === 'boolean') {
+    return '' + obj;    
+  }
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }   
+  if (Array.isArray(obj)) {
+    result = '[';
+    obj.forEach(elem => {
+      result += stringifyJSON(elem);
+      result += ',';
+    });
+    if (obj.length > 0) {
+      result = result.slice(0, result.length - 1);
+    }
+    result += ']';
+    return result;
+  }
+  // if (typeof obj === 'object') {
+  //   result = '{';
+  //   for (let key in obj) {
+  //     result += stringifyJSON(key) + ':' stringifyJSON(obj[key]) + ',';
+  //   }
+  //   if (result.length !== 2) {
+  //     result = result.slice(0, result.length - 1);
+  //   }
+  // }
+
 };
