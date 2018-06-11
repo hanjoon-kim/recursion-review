7c17a29f5fd1911b9ddd365a52ad37a91f009d9d
hanjoon-kim <hanjoon3@gmail.com>
2018-06-11 15:38:51 -0700

    Fix syntax errors, but still unexpected results

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node = document.body) {
+  // input: a target class name as a string
+  // output: an nodelist of all elements that have the classname
+  // strategy: have an array of results, and concatenate to current list
+
+  // create an array to keep track of nodes with the target
+  var results = [];
+  // check if classname is in current node
+  if (node.classList !== undefined && node.classList.contains(className)) {
+    // concantenate to result array
+    results.push(node);
+  }
+  // check if node has children
+  if (node.hasChildNodes()) {
+    // iterate through each child
+    for (let i = 0; i < node.childNodes.length; i++) {
+      results.concat(getElementsByClassName(className, node.childNodes[i]));
+    }
+  }
+  // return the array
+  return results; 
 };
